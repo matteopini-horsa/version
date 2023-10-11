a405dbaba381dafae5f3466bd905126f813809b7
matteo pini <matteo.pini@horsa.it>
2023-10-11 15:51:15 +0200

    x

--- custom-version-updater.js
+++ custom-version-updater.js
@@ -2,15 +2,19 @@
 const stringifyPackage = require('stringify-package')
 const detectIndent = require('detect-indent')
 const detectNewline = require('detect-newline')
-
+console.log('custom-version-updater')
 module.exports.readVersion = function (contents) {
-  const matches = contents.match(/window\.SUP\.version = '(\d{1,}\.\d{1,}\.\d{1,})'/);
-  return matches[1];
+    const matches = contents.match(/window\.SUP\.version = '(\d{1,}\.\d{1,}\.\d{1,})'/);
+    console.log('***', contents);
+    console.log('***', matches[1])
+    return matches[1];
 }
 
 module.exports.writeVersion = function (contents, version) {
-contents.replace(/window\.SUP\.version = '(\d{1,}\.\d{1,}\.\d{1,})'/, function(match, p1) {
-    return match.replace(p1, version);
-})
-  return contents;
+    console.log('???', contents);
+    console.log('???', version);
+    contents.replace(/window\.SUP\.version = '(\d{1,}\.\d{1,}\.\d{1,})'/, function (match, p1) {
+        return match.replace(p1, version);
+    })
+    return contents;
 }
